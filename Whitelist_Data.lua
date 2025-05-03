0a5ba8db21353cb9c6c4867bf312f4ba461eee24
IlIIlllllII <yozyyozy251@gmail.com>
2025-05-03 11:13:38 -0500

    Update and rename whitelist_Data.lua to Whitelist_Data.lua

---
+++ Whitelist_Data.lua
@@ -0,0 +1,7 @@
+-- Whitelist Data
+-- This file contains the list of whitelisted users
+
+return {
+    -- Add your whitelist strings here after running the script once
+    -- "Lurnai_1234567890_...",
+}
