e93bdfa5e30a8bfec61bdf4e1d3270a103a53708
IlIIlllllII <yozyyozy251@gmail.com>
2025-05-03 16:59:58 -0500

    Update and rename WWhitelist_Data.lua to Whitelist_Data.lua

---
+++ Whitelist_Data.lua
@@ -0,0 +1,8 @@
+-- Whitelist Data
+-- This file contains the list of whitelisted users
+
+return {
+    -- Add your whitelist strings here after running the script onc
+    "##SECURE##<<TIME>>BHEGDAHIAF<</TIME>>EbVx_DaUw-Pm6=+Mj3):Nk4-.Mj3)_GdXz-Ol5++Pm6=:Nk4-.Jg0&_DaUw-Kh1*+Pm6=:HeY%.Tq!}_FcWy-Mj3)+Pm6=:Jg0&.Ct$v_Qn7[-FcWy+Ct$v:Tq!}.Nk4-_Qn7[-GdXz+IfZ^:DaUw.FcWy_Pm6=-Ol5++Ct$v:Pm6=.HeY%_Ol5+-Ol5++FcWy:FcWy.DaUw_Qn7[-HeY%+Sp9{:Pm6=.Sp9{_Kh1*-GdXz+Sp9{:Jg0&.Tq!}_HeY%-GdXz+Ct$v:Jg0&.Ar@;_Tq!}-Bs#u+GdXz:Sp9{.Kh1*_HeY%-Sp9{+Sp9{:Jg0&.HeY%_Nk4--Ro8]+Ro8]:EbVx.EbVx_Ar@;-IfZ^+Sp9{:DaUw.FcWy_GdXz-Li2(+DaUw:Mj3).HeY%_Li2(-Nk4-+Qn7[:Bs#u.Jg0&_Ct$v-Ct$v+Pm6=:Li2(.FcWy_Ar@;-IfZ^+Jg0&:Sp9{.Sp9{_Ro8]-DaUw+Pm6=:Tq!}.Pm6=_Qn7[-Sp9{+Ol5+:Jg0&.IfZ^_FcWy-DaUw+Qn7[:FcWy.Ro8]_Ct$v-Sp9{+Ol5+:Tq!}.GdXz_Ro8]-DaUw+Qn7[:Ol5+.Kh1*_Ct$v-IfZ^+Bs#u:Qn7[.Kh1*_EbVx-Jg0&+Pm6=:Tq!}.Mj3)_Jg0&-Qn7[+Ct$v:HeY%.Ol5+_Ro8]-Ar@;+EbVx:IfZ^.Bs#u_Mj3)-Ct$v+Li2(:EbVx.Ro8]_Ro8]-Ct$v+Bs#u:Pm6=.HeY%_Ro8]-Ct$v+Ro8]:Mj3).Ct$v_DaUw-Kh1*+DaUw:Sp9{.Tq!}_Pm6=-EbVx+HeY%:Jg0&.Sp9{_Tq!}-EbVx+Li2(:FcWy.Mj3)_IfZ^-<<VERIFY>>BFFGI<</VERIFY>>"
+       
+}
